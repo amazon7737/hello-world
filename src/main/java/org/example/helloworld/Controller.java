6b3c6c0e5aeba8703a2d6fa37816a51c950115a9
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-07-03 22:43:12 +0900

    feat: log 레벨 변경, 로그 생성

--- src/main/java/org/example/helloworld/Controller.java
+++ src/main/java/org/example/helloworld/Controller.java
@@ -1,15 +1,19 @@
 package org.example.helloworld;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/")
+@RequestMapping("/hello")
 public class Controller {
 
-    @GetMapping
-    public String load() {
+    @GetMapping("/load")
+    public String load(@RequestParam(value = "name") String name) {
+        log.info("hello[{}]", name);
         return "hello world?";
     }
 }
